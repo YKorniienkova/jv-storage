00dac226ae87d6d92e374263f72c69f3277226ca
Yelyzaveta Korniienkova <yelyzavetakorniienkova@gmail.com>
2026-02-09 12:36:12 +0100

    fixed problems

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -34,7 +34,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(keys, key)) {
+            if (Objects.equals(keys[i], key)) {
                 return values[i];
             }
         }
